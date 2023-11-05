56ec2a1e659b41d53284ad11fd4aec4a7f69deb9
jjkim0816 <jjkim0816@gmail.com>
2023-11-05 11:38:20 +0900

    first commit

---
+++ src/main/java/com/example/project_actual_dividend/ProjectActualDividendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_actual_dividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectActualDividendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectActualDividendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_actual_dividend/ProjectActualDividendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_actual_dividend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectActualDividendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
